a521b2e36f2cc18670cf64f899ea48a3c68696b2
HelloAsher <281664501@qq.com>
2017-09-21 16:27:38 +0800

    增加了ExecutionHandler

---
+++ bt/components/execution_handler/execution.py
@@ -0,0 +1,39 @@
+import datetime
+from queue import Queue
+
+from abc import ABCMeta, abstractclassmethod
+
+from bt.components.event.event import OrderEvent, FillEvent
+
+
+class ExecutionHandler(metaclass=ABCMeta):
+    """
+    ExecutionHandler是用来处理portfolio和市场数据的相互交互的，填充FillEvent
+    """
+
+    @abstractclassmethod
+    def execute_order(self, event: OrderEvent):
+        """
+        消费一个OrderEvent，然后产生一个FillEvent，然后将这FillEvent放到Event Queue中
+        :param event:   OrderEvent
+        :return:
+        """
+        raise NotImplementedError("Should implement execute_order()")
+
+
+class SimulatedExecutionHandler(ExecutionHandler):
+    """
+    1、SimulatedExecutionHandler只是把一个OrderEvent转化成了FillEvent，这只是简单的转化，本类中没有考虑延迟、滑点和填充率的问题
+    2、在实现一个复杂的执行系统之前，这个简单的ExecutionHandler对任何一个策略都是到来就立即执行
+    """
+
+    def __init__(self, events: Queue):
+        self.events = events
+        pass
+
+    def execute_order(self, event: OrderEvent):
+        if event.type == "ORDER":
+            fill_event = FillEvent(datetime.datetime.utcnow(), event.symbol, "SH", event.quantity, event.direction,
+                                   None)
+            self.events.put(fill_event)
+        pass
